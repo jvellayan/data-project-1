80c8b276cda196ea4bf8e8bfba9172a562aea9c3
Jaya Vellayan <jlv7cr@virginia.edu>
2021-04-13 10:30:02 -0400

    commit code

--- main.py
+++ main.py
@@ -16,6 +16,12 @@ df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 print("Total number of records: ", df.shape[0])
 # get total number of columns
 print("Total number of columns: ", len(df.columns))
+# get the total number of NAs
+print("Total number of NA: ", df.isna().sum().sum())
+
+# drop all rows with NA, usually with as many NAs as this df has, we would try to salvage some of this data,
+# but dropped it for simplicity
+df = df.dropna()
 
 # add a column that lists price per room
 df["Price per room"] = df["Price"]/df["Rooms"]
@@ -30,6 +36,10 @@ del df['Bedroom2']
 
 # get total number of columns
 print("New number of columns after adding and deleting columns: ", len(df.columns))
+# get total number of records
+print("New number of records after dropping NA: ", df.shape[0])
+# get the total number of NAs
+print("New number of NA: ", df.isna().sum().sum())
 
 # convert data frame to json
 df.to_json("data/melb_data.json")
