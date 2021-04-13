cf0b7c67b00970d96a5b0758136324a4c84aeaa0
Jaya Vellayan <jlv7cr@virginia.edu>
2021-04-13 14:55:18 -0400

    commit code

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # import pandas to work with data frames
 import pandas as pd
+# import requests and io to handle getting data from a url
 import requests
 import io
 
 # get csv from url
 url = 'https://raw.githubusercontent.com/jvellayan/data-project-1/main/data/melb_data.csv'
 
-# Reading the downloaded content and turning it into a pandas dataframe
+# reading the downloaded content
 download = requests.get(url).content
 
-# convert csv to dataframe
+# convert to dataframe
 df = pd.read_csv(io.StringIO(download.decode('utf-8')))
 
 # get total number of records
@@ -18,6 +19,15 @@ print("Total number of records: ", df.shape[0])
 print("Total number of columns: ", len(df.columns))
 # get the total number of NAs
 print("Total number of NA: ", df.isna().sum().sum())
+
+# get column type information
+print("Column Type: ")
+print(df.info())
+# more stats
+print("Count, mean, std, min, max and distribution stats for each column: ")
+with pd.option_context('display.max_columns', 40):
+    print(df.describe(include='all'))
+
 
 # drop all rows with NA, usually with as many NAs as this df has, we would try to salvage some of this data,
 # but dropped it for simplicity
@@ -43,3 +53,4 @@ print("New number of NA: ", df.isna().sum().sum())
 
 # convert data frame to json
 df.to_json("data/melb_data.json")
+
